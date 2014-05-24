bba77bb45a0d59278f4228ba6ad193a335aa447b
Bradlee Speice <bspeice.nc@gmail.com>
2014-05-24 02:13:35 -0400

    And add an incredibly basic test

---
+++ MinimalBible/src/test/java/org/bspeice/minimalbible/test/DownloadActivityTest.java
@@ -0,0 +1,15 @@
+package org.bspeice.minimalbible.test;
+
+import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+/**
+ * Tests for the Download activity
+ */
+public class DownloadActivityTest extends InstrumentationTestCase {
+
+    public void testBasicAssertion() {
+        assertEquals(true, true);
+    }
+
+}
